640140fffd7acac8421c3c48f871cfb5df9c88c4
Md Nafeed Razy Akram <nafeedjmi@gmail.com>
2024-06-30 09:43:39 +0530

    refs >> Working

--- JS-Topics/promise.js
+++ JS-Topics/promise.js
@@ -1,5 +1,7 @@
 //Promises are a way to handle asynchronous operations in JavaScript
 
+// https://github.com/mdnafeed/JavaScript-Coding-and-Notes/blob/main/JS-Topics/promise.js
+
 // Advantages of Promises
 
 // 1. Improved Readability
@@ -84,3 +86,72 @@
 // console.log(fetchPromise);
 
 // ----------------------------------------------------------------------
+
+// example 3
+/*
+new Promise((resolve, reject) => {
+    console.log("promise")
+    try {
+        throw new Error("Something is wrong!");
+    }
+    catch(e){
+        console.log(e.message)
+    }
+}).catch((error) => console.log(error)); 
+*/
+
+// Example 4
+
+// Handle Multiple Promises
+
+// Promise.race, Promise.all, Promise.any, Promise.allSettled, Promise.any
+
+// Promise.any
+
+// CASE 1: This method takes an array of promises as input and returns a single promise that
+// CASE 2: It will resolve if any of the promises in an array are resolved with the resolved value
+// CASE 3: If all the promises are rejected. promise will be rejected with the error “AggregateError: All promises were rejected”.
+
+var promise1 = new Promise((resolve,reject) =>{
+    setTimeout(() => resolve('Promise 1 resolved'), 1000);
+});
+
+var promise2 = new Promise((resolve,reject) =>{
+    setTimeout(() => resolve('Promise 2 resolved'), 1000);
+});
+
+var promise3 = new Promise((resolve,reject) =>{
+    setTimeout(() => resolve('Promise 3 resolved'), 1000);
+});
+
+// const mergePromise = Promise.any([promise1,promise2,promise3])
+//                     .then((data) =>{
+//                         console.log(data)
+//                     })
+//                     .catch((error) =>{
+//                         console.log(error)
+//                     })
+//                     .finally(() =>{
+//                         console.log("Finally Block is be executed")
+//                     })
+
+// console.log(mergePromise)
+
+// -------------------------------------------------------------------------------
+
+// 2. Promise.all
+
+// CASE 1. It is a static method 
+// CASE 2. This method takes an array of promises as input and returns a single promise that
+// CASE 3. resolves when all of the promises in the array have resolved, or rejects when any one of the promises rejects.
+
+const allPromise = Promise.all([promise1,promise2,promise3])
+                    .then((data)=>{
+                        console.log(data);
+                    }).
+                    catch((error) =>{
+                        console.log(error);
+                    })
+                    .finally(() =>{
+                        console.log("Finally Block will be executed");
+                    })
